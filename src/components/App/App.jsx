28e5e94c066f4295a4bf5b83a7e2debe87ad6f7e
Angie <angelinamenendez@hotmail.com>
2025-03-03 17:21:14 -0300

    creacion Layout

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+function App() {
+  return (<h1>Hola a mi proyecto</h1>)
+}
+
+export default App
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,30 @@
+import "./Layout.css"
+
+const Layout = () => {
+    return (
+        <>
+            <header>
+                <img src="https://tse3.mm.bing.net/th?id=OIP.3oSANImpLLxEuIZbH33wQAHaHa&pid=Api&P=0&h=180"
+                    alt="Logo" />
+                <p className="frase-header">¡Pinta tu vida de colores!</p>
+                <nav>
+                    <ul>
+                        <li>NOSOTROS</li>
+                        <li>COLORES</li>
+                        <li>CONTACTO</li>
+                    </ul>
+                </nav>
+            </header >
+
+            <main>
+                <p>PROXIMAMENTE...</p>
+            </main>
+
+            <footer>
+                <p> .Sitio desarrollado por Angelina Josefa Menendez.</p>
+            </footer>
+        </>
+    )
+}
+
+export { Layout }
